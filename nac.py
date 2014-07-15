87a8839df40cd93796ae816df811e35ea110cf4c
Matthew Tylee Atkinson <matatk@gmail.com>
2014-07-15 13:45:58 +0100

    RED add move lower bound test

--- nac.py
+++ nac.py
@@ -1,3 +1,8 @@
 def test_legal_move():
     last_move_was_legal = True
     assert last_move_was_legal
+
+def test_move_bounds_lower():
+    move = -1
+    last_move_was_legal = move < 0
+    assert last_move_was_legal is False
