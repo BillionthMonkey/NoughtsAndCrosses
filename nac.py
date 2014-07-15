29c259020b735c28052e87db9978cdf1aa04c3fa
Matthew Tylee Atkinson <matatk@gmail.com>
2014-07-15 13:54:35 +0100

    RED check legal on lower boundary

--- nac.py
+++ nac.py
@@ -4,6 +4,9 @@ def test_legal_move():
 def test_move_bounds_lower():
     assert is_legal(-1) is False
 
+def test_move_lower_boundary():
+    assert is_legal(0) is True
+
 
 def is_legal(move):
     return move > 0
