6e24e2ef1e54fe4636f3fa4170675878eb2b811c
Matthew Tylee Atkinson <matatk@gmail.com>
2014-07-15 13:30:26 +0100

    RED add test for illegal move

--- nac.py
+++ nac.py
@@ -1,3 +1,7 @@
 def test_legal_move():
     last_move_was_legal = True
     assert last_move_was_legal
+
+def test_illegal_move():
+    last_move_was_legal = True
+    assert last_move_was_legal is False
