008be46db9a50615c993aa72514fc5d9836a8de9
Matthew Tylee Atkinson <matatk@gmail.com>
2014-07-16 13:48:23 +0100

    REFACTOR introduced instance variable

--- nac.py
+++ nac.py
@@ -1,22 +1,26 @@
 def test_move_is_legal_if_not_already_played():
 	nac = NaughtsAndCrosses()
-	assert nac.is_legal(0, []) is True
+	assert nac.is_legal(0) is True
 
 def test_move_is_not_legal_if_already_played():
 	nac = NaughtsAndCrosses()
-	assert nac.is_legal(0, [0]) is False
+	nac.moves_played_so_far = [0]
+	assert nac.is_legal(0) is False
 
 def test_move_is_not_legal_if_below_lower_bound():
 	nac = NaughtsAndCrosses()
-	assert nac.is_legal(-1, []) is False
+	assert nac.is_legal(-1) is False
 
 def test_move_is_not_legal_if_above_upper_bound():
 	nac = NaughtsAndCrosses()
-	assert nac.is_legal(9, []) is False
+	assert nac.is_legal(9) is False
 
 
 class NaughtsAndCrosses:
-	def is_legal(self, move, moves_played_so_far):
+	def __init__(self):
+		self.moves_played_so_far = []
+
+	def is_legal(self, move):
 		move_within_bounds = move > -1 and move < 9
-		move_not_played = move not in moves_played_so_far
+		move_not_played = move not in self.moves_played_so_far
 		return move_within_bounds and move_not_played
