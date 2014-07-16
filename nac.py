0bfd7b2e8ba1f44babad3448e3d3f6749029fb56
Matthew Tylee Atkinson <matatk@gmail.com>
2014-07-16 13:34:22 +0100

    REFACTOR extracted method

--- nac.py
+++ nac.py
@@ -1,11 +1,9 @@
 def test_move_is_legal_if_not_already_played():
-	move = 0
-	moves_played_so_far = []
-	is_legal = move not in moves_played_so_far
-	assert is_legal is True
+	assert is_legal(0, []) is True
 
 def test_move_is_not_legal_if_already_played():
-	move = 0
-	moves_played_so_far = [0]
-	is_legal = move not in moves_played_so_far
-	assert is_legal is False
+	assert is_legal(0, [0]) is False
+
+
+def is_legal(move, moves_played_so_far):
+	return move not in moves_played_so_far
