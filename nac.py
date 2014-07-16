e9d1a7862b19c350ad87d7fcaee81414938066bf
Matthew Tylee Atkinson <matatk@gmail.com>
2014-07-16 13:18:25 +0100

    RED add test for not legal move

--- nac.py
+++ nac.py
@@ -3,3 +3,6 @@ def test_move_is_legal_if_not_already_played():
 	moves_played_so_far = []
 	is_legal = move not in moves_played_so_far
 	assert is_legal is True
+
+def test_move_is_not_legal_if_already_played():
+	assert 0 not in [0]
