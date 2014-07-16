4a204f5f241b4b1d71ebc0aa8de059bb70f5adc0
Matthew Tylee Atkinson <matatk@gmail.com>
2014-07-16 13:36:39 +0100

    RED move lower bound check

--- nac.py
+++ nac.py
@@ -4,6 +4,9 @@ def test_move_is_legal_if_not_already_played():
 def test_move_is_not_legal_if_already_played():
 	assert is_legal(0, [0]) is False
 
+def test_move_is_not_legal_if_below_lower_bound():
+	assert is_legal(-1, []) is False
+
 
 def is_legal(move, moves_played_so_far):
 	return move not in moves_played_so_far
