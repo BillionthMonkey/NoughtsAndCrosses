1c65435920dcd9141c9c052b0c2192a8cd7469a4
Matthew Tylee Atkinson <matatk@gmail.com>
2014-07-16 13:16:13 +0100

    REFACTOR extract another local variable ;-)

--- nac.py
+++ nac.py
@@ -1,4 +1,5 @@
 def test_move_is_legal_if_not_already_played():
 	move = 0
 	moves_played_so_far = []
-	assert move not in moves_played_so_far
+	is_legal = move not in moves_played_so_far
+	assert is_legal is True
