ef9c6c3d22047d811f5f61a38e5e9afecfdafc28
Matthew Tylee Atkinson <matatk@gmail.com>
2014-07-17 14:12:16 +0100

    GREEN

--- nac.py
+++ nac.py
@@ -58,7 +58,8 @@ class NoughtsAndCrosses:
 		top_row = set([0, 1, 2])
 		middle_row = set([3, 4, 5])
 		return self._is_win_for_player_one(top_row) \
-			or self._is_win_for_player_one(middle_row)
+			or self._is_win_for_player_one(middle_row) \
+			or self._is_win_for_player_one(set([6, 7, 8]))
 
 	def _is_win_for_player_one(self, pattern):
 		player_one_moves = set(self.moves_played_so_far[0::2])
