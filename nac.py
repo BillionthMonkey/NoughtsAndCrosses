084b107bc88c6685e10b1e10d2fe05cb3da43ed6
Matthew Tylee Atkinson <matatk@gmail.com>
2014-07-17 13:44:23 +0100

    REFACTOR some renames

--- nac.py
+++ nac.py
@@ -1,37 +1,37 @@
 def test_move_is_legal_if_not_already_played():
-	nac = NaughtsAndCrosses()
+	nac = NoughtsAndCrosses()
 	assert nac.is_legal(0) is True
 
 def test_move_is_not_legal_if_already_played():
-	nac = NaughtsAndCrosses()
+	nac = NoughtsAndCrosses()
 	nac.moves_played_so_far = [0]
 	assert nac.is_legal(0) is False
 
 def test_move_is_not_legal_if_below_lower_bound():
-	nac = NaughtsAndCrosses()
+	nac = NoughtsAndCrosses()
 	assert nac.is_legal(-1) is False
 
 def test_move_is_not_legal_if_above_upper_bound():
-	nac = NaughtsAndCrosses()
+	nac = NoughtsAndCrosses()
 	assert nac.is_legal(9) is False
 
 def test_draw():
-	nac = NaughtsAndCrosses()
+	nac = NoughtsAndCrosses()
 	nac.moves_played_so_far = [ 0, 1, 2, 3, 4, 5, 6, 7, 8 ]
 	assert nac.is_draw() is True
 
 def test_not_draw():
-	nac = NaughtsAndCrosses()
+	nac = NoughtsAndCrosses()
 	nac.moves_played_so_far = [ 0, 1, 2, 3, 4, 5, 6, 7 ]
 	assert nac.is_draw() is False
 
-def test_top_row_filled_is_win():
-	nac = NaughtsAndCrosses()
+def test_top_row_filled_player_one_is_win():
+	nac = NoughtsAndCrosses()
 	nac.moves_played_so_far = [ 0, 7, 1, 8, 2 ]
 	assert set([0, 1, 2]).issubset(set(nac.moves_played_so_far[0::2]))
 
 
-class NaughtsAndCrosses:
+class NoughtsAndCrosses:
 	def __init__(self):
 		self.moves_played_so_far = []
 		self.max_number_of_moves = 9
