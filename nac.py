cf59aee86915a5bda4c8e0dbcc34df85dfbc3877
Matthew Tylee Atkinson <matatk@gmail.com>
2014-07-22 13:34:02 +0100

    REFACTOR extract local variable

--- nac.py
+++ nac.py
@@ -64,11 +64,12 @@ class NoughtsAndCrosses:
 		middle_row = set([3, 4, 5])
 		bottom_row = set([6, 7, 8])
 		first_column = set([0, 3, 6])
+		second_column = set([1, 4, 7])
 		return self._is_win_for_player_one(top_row) \
 			or self._is_win_for_player_one(middle_row) \
 			or self._is_win_for_player_one(bottom_row) \
 			or self._is_win_for_player_one(first_column) \
-			or self._is_win_for_player_one(set([1, 4, 7]))
+			or self._is_win_for_player_one(second_column)
 
 	def _is_win_for_player_one(self, pattern):
 		player_one_moves = set(self.moves_played_so_far[0::2])
