3eea65b3df5b3d4219682b192721868218ce54f5
Matthew Tylee Atkinson <matatk@gmail.com>
2014-07-30 13:52:38 +0100

    RED top row in a different order

--- nac.py
+++ nac.py
@@ -2,3 +2,8 @@ def test_complete_top_row_is_win():
     moves = [0, 1, 2]
     top_row = [0, 1, 2]
     assert moves == top_row
+
+def test_complete_top_row_in_different_order_is_win():
+    moves = [2, 1, 0]
+    top_row = [0, 1, 2]
+    assert moves == top_row
