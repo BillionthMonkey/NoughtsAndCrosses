8f588d0db18b556a643e80900e265eca29f61e7f
Matthew Tylee Atkinson <matatk@gmail.com>
2014-07-30 14:36:06 +0100

    RED multiple winning options

--- nac.py
+++ nac.py
@@ -1,3 +1,11 @@
 def test_win():
     moves = [2, 1, 3, 0]
     assert set(moves).issuperset(set([0, 1, 2]))
+
+def test_win_with_two_options():
+    moves = [2, 1, 3, 0]
+    options = [
+        [0, 1, 2],
+        [3, 4, 5]
+    ]
+    assert any([moves == option for option in options])
