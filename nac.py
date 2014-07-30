da1c3fa102506c4f5c1a8199087ba8f3fb50d88b
Matthew Tylee Atkinson <matatk@gmail.com>
2014-07-30 14:40:20 +0100

    REFACTOR extract method

--- nac.py
+++ nac.py
@@ -1,6 +1,9 @@
 def test_win():
     moves = [2, 1, 3, 0]
-    assert set(moves).issuperset(set([0, 1, 2]))
+    options = [
+        [0, 1, 2]
+    ]
+    assert is_win(moves, options) is True
 
 def test_win_with_two_options():
     moves = [2, 1, 3, 0]
@@ -8,5 +11,9 @@ def test_win_with_two_options():
         [0, 1, 2],
         [3, 4, 5]
     ]
-    assert any([set(moves).issuperset(set(option))
+    assert is_win(moves, options) is True
+
+
+def is_win(moves, options):
+    return any([set(moves).issuperset(set(option))
         for option in options])
