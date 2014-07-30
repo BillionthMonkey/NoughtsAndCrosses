541a453ba2beb52fb9a9ef0b5efa06bb59d7da55
Matthew Tylee Atkinson <matatk@gmail.com>
2014-07-30 14:06:33 +0100

    REFACTOR moved code to is_win

--- nac.py
+++ nac.py
@@ -12,9 +12,10 @@ def test_more_than_complete_top_row_is_win():
 
 def test_complete_middle_row_is_win():
     moves = [3, 4, 5]
-    assert (is_win(moves) or moves == [3, 4, 5]) is True
+    assert is_win(moves) is True
 
 
 def is_win(moves):
     top_row = set([0, 1, 2])
-    return set(moves).issuperset(top_row)
+    return set(moves).issuperset(top_row) \
+            or moves == [3, 4, 5]
