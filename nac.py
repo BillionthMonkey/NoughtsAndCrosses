7d21444b049fc14f93d44cca17030416290adf4f
Matthew Tylee Atkinson <matatk@gmail.com>
2014-07-30 14:01:00 +0100

    RED middle row

--- nac.py
+++ nac.py
@@ -10,6 +10,10 @@ def test_more_than_complete_top_row_is_win():
     moves = [0, 1, 2, 3]
     assert is_win(moves) is True
 
+def test_complete_middle_row_is_win():
+    moves = [3, 4, 5]
+    assert is_win(moves) is True
+
 
 def is_win(moves):
     top_row = set([0, 1, 2])
