45af8dbbc8f298dbaf09108b6a2738a87724c0ff
Matthew Tylee Atkinson <matatk@gmail.com>
2014-08-06 13:13:44 +0100

    REFACTOR extract player moves method

--- nac.py
+++ nac.py
@@ -15,15 +15,16 @@ def test_win_with_two_options():
 
 def test_extract_player_one_moves():
     moves = [2, 7, 1, 8, 3]
-    player_one_moves = moves[0::2]
-    assert player_one_moves == [2, 1, 3]
+    assert moves_for_player(0, moves) == [2, 1, 3]
 
 def test_extract_player_two_moves():
     moves = [4, 5, 6, 7, 8]
-    player_two_moves = moves[1::2]
-    assert player_two_moves == [5, 7]
+    assert moves_for_player(1, moves) == [5, 7]
 
 
 def is_win(moves, options):
     return any([set(moves).issuperset(set(option))
         for option in options])
+
+def moves_for_player(player, moves):
+    return moves[player::2]
