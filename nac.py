4775c7c4b538f08b929800045e2a670220222d8c
Matthew Tylee Atkinson <matatk@gmail.com>
2014-08-06 13:10:54 +0100

    RED extract player two moves

--- nac.py
+++ nac.py
@@ -18,6 +18,11 @@ def test_extract_player_one_moves():
     player_one_moves = moves[0::2]
     assert player_one_moves == [2, 1, 3]
 
+def test_extract_player_two_moves():
+    moves = [4, 5, 6, 7, 8]
+    player_two_moves = moves
+    assert player_two_moves == [5, 7]
+
 
 def is_win(moves, options):
     return any([set(moves).issuperset(set(option))
