d1d4bc8be9a546bf52bdde14087fc307cf9bfa55
Matthew Tylee Atkinson <matatk@gmail.com>
2014-08-06 13:08:31 +0100

    GREEN

--- nac.py
+++ nac.py
@@ -15,7 +15,7 @@ def test_win_with_two_options():
 
 def test_extract_player_one_moves():
     moves = [2, 7, 1, 8, 3]
-    player_one_moves = moves
+    player_one_moves = moves[0::2]
     assert player_one_moves == [2, 1, 3]
 
 
