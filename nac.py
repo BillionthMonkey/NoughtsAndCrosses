8967cda5436f805864c3dc7f76e7043a9fd7e0a2
Matthew Tylee Atkinson <matatk@gmail.com>
2014-08-06 13:16:38 +0100

    REFACTOR rename tests

--- nac.py
+++ nac.py
@@ -13,11 +13,11 @@ def test_win_with_two_options():
     ]
     assert is_win(moves, options) is True
 
-def test_extract_player_one_moves():
+def test_extract_first_player_moves():
     moves = [2, 7, 1, 8, 3]
     assert moves_for_player(0, moves) == [2, 1, 3]
 
-def test_extract_player_two_moves():
+def test_extract_second_player_moves():
     moves = [4, 5, 6, 7, 8]
     assert moves_for_player(1, moves) == [5, 7]
 
