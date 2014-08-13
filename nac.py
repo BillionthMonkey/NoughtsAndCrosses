470ac7268f462d185595c62bb3267b47df1ba73a
Matthew Tylee Atkinson <matatk@gmail.com>
2014-08-13 14:21:42 +0100

    RED added model to controller

--- nac.py
+++ nac.py
@@ -6,6 +6,12 @@ def test_playing_legal_move_updates_view():
     controller.play_move(0)
     fake_view.add_move.assert_called_with(0)
 
+def test_playing_illegal_move_does_not_update_view():
+    fake_view = mock.Mock()
+    fake_model = mock.Mock()
+    fake_model.is_legal.return_value = False
+    controller = NoughtsAndCrossesController(fake_model, fake_view)
+
 class NoughtsAndCrossesController:
     def __init__(self, view):
         self.view = view
