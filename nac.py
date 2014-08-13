3c81990de9ebc8f07401d7d6bac8ab079a772877
Matthew Tylee Atkinson <matatk@gmail.com>
2014-08-13 14:15:35 +0100

    RED test view is called

--- nac.py
+++ nac.py
@@ -1,6 +1,10 @@
+import mock
+
 def test_playing_legal_move_updates_view():
+    fake_view = mock.Mock()
     controller = NoughtsAndCrossesController()
     controller.play_move(0)
+    fake_view.add_move.assert_called_with(0)
 
 class NoughtsAndCrossesController:
     def play_move(self, move):
