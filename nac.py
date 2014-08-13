01f68b7c14dc608c2e58a53d583c3ef96098ff24
Matthew Tylee Atkinson <matatk@gmail.com>
2014-08-13 14:17:15 +0100

    GREEN

--- nac.py
+++ nac.py
@@ -2,10 +2,13 @@ import mock
 
 def test_playing_legal_move_updates_view():
     fake_view = mock.Mock()
-    controller = NoughtsAndCrossesController()
+    controller = NoughtsAndCrossesController(fake_view)
     controller.play_move(0)
     fake_view.add_move.assert_called_with(0)
 
 class NoughtsAndCrossesController:
+    def __init__(self, view):
+        self.view = view
+
     def play_move(self, move):
-        pass
+        self.view.add_move(move)
