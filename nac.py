6f9993a0b9ab6b134eff5f6395cdf31314bac790
Matthew Tylee Atkinson <matatk@gmail.com>
2014-09-23 12:48:35 +0100

    REFACTOR use a test class

--- nac.py
+++ nac.py
@@ -1,19 +1,24 @@
 import mock
 
-def test_playing_legal_move_updates_view():
-    fake_view = mock.Mock()
-    fake_model = mock.Mock()
-    controller = NoughtsAndCrossesController(fake_model, fake_view)
-    controller.play_move(0)
-    fake_view.add_move.assert_called_with(0)
+class TestNoughtsAndCrosses():
+    def setup_method(self, method):
+        self.fake_view = mock.Mock()
+        self.fake_model = mock.Mock()
+        self.controller = NoughtsAndCrossesController(
+            self.fake_model,
+            self.fake_view)
 
-def test_playing_illegal_move_does_not_update_view():
-    fake_view = mock.Mock()
-    fake_model = mock.Mock()
-    fake_model.is_legal.return_value = False
-    controller = NoughtsAndCrossesController(fake_model, fake_view)
-    controller.play_move(42)
-    assert not fake_view.add_move.called
+    def teardown_method(self, method):
+        pass
+
+    def test_playing_legal_move_updates_view(self):
+        self.controller.play_move(0)
+        self.fake_view.add_move.assert_called_with(0)
+
+    def test_playing_illegal_move_does_not_update_view(self):
+        self.fake_model.is_legal.return_value = False
+        self.controller.play_move(42)
+        assert not self.fake_view.add_move.called
 
 class NoughtsAndCrossesController:
     def __init__(self, model, view):
