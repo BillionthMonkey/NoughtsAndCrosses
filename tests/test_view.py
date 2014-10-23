70d9455fb9484b9435165bad006a775509d8a4ba
Matthew Tylee Atkinson <matatk@gmail.com>
2014-10-23 13:21:50 +0100

    RED 'win' method should emit congratulatory message.

--- tests/test_view.py
+++ tests/test_view.py
@@ -1,6 +1,11 @@
+import sys
+import mock
 from nac.view import NoughtsAndCrossesView
 
 class TestNoughtsAndCrossesView():
     def test_win(self):
+        fake_stdout = mock.Mock()
+        sys.stdout = fake_stdout
         view = NoughtsAndCrossesView()
         view.win()
+        fake_stdout.write.assert_any_call('Congratulations; you won!')
