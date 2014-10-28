aa8888c144ddb72d10d591c6a1931d31b99744b5
Matthew Tylee Atkinson <matatk@gmail.com>
2014-10-28 13:47:33 +0000

    REFACTOR DRY.

--- tests/test_view.py
+++ tests/test_view.py
@@ -5,21 +5,19 @@ from nac.view import NoughtsAndCrossesView
 class TestNoughtsAndCrossesView():
     def setup_method(self, method):
         self.fake_stdout = mock.Mock()
+        self.view = NoughtsAndCrossesView()
 
     def test_win(self, capsys):
-        view = NoughtsAndCrossesView()
-        view.win()
+        self.view.win()
         self._assert_stdout_is('Congratulations; you won!\n', capsys)
 
     def test_draw(self, capsys):
-        view = NoughtsAndCrossesView()
-        view.draw()
+        self.view.draw()
         self._assert_stdout_is("It's a draw!\n", capsys)
 
     def test_report_error(self, capsys):
-        view = NoughtsAndCrossesView()
         message = 'Illegal move.'
-        view.report_error(message)
+        self.view.report_error(message)
         self._assert_stdout_is(message + '\n', capsys)
 
     def _assert_stdout_is(self, expected, capsys):
