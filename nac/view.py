67916d07eeb607b7ffeaa1551dd6f04d4838a4d7
Matthew Tylee Atkinson <matatk@gmail.com>
2014-11-13 13:23:01 +0000

    GREEN

--- nac/view.py
+++ nac/view.py
@@ -12,7 +12,7 @@ class NoughtsAndCrossesView():
         self._board_renderer.render([])
 
     def add_move(self, move):
-        self._board_renderer.render([0])
+        self._board_renderer.render([move])
 
     def report_error(self, message):
         print message
