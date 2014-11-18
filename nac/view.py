5f5469b1a92c03e49a5a8b0fa62c0c22430ee600
Matthew Tylee Atkinson <matatk@gmail.com>
2014-11-18 13:10:00 +0000

    GREEN

--- nac/view.py
+++ nac/view.py
@@ -1,6 +1,7 @@
 class NoughtsAndCrossesView():
     def __init__(self, board_renderer):
         self._board_renderer = board_renderer
+        self._moves = []
 
     def win(self):
         print 'Congratulations; you won!'
@@ -12,7 +13,8 @@ class NoughtsAndCrossesView():
         self._board_renderer.render([])
 
     def add_move(self, move):
-        self._board_renderer.render([move])
+        self._moves.append(move)
+        self._board_renderer.render(self._moves)
 
     def report_error(self, message):
         print message
