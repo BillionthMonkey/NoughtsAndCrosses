290e42cc7cbca27100bc756189a75f25b5675fa2
Matthew Tylee Atkinson <matatk@gmail.com>
2014-11-19 15:44:17 +0000

    REFACTOR pull out row size and row-starting cells as fields.

--- nac/view.py
+++ nac/view.py
@@ -25,16 +25,16 @@ class BoardRenderer():
         self._number_of_players = 2
         self._player_one = 0
         self._player_two = 1
+        self._row_size = 3
+        self._starts_of_rows = [0, 3, 6]
 
     def render(self, moves):
         cells = [' '] * 9
         self._update_cells(self._player_one, moves, cells)
         self._update_cells(self._player_two, moves, cells)
-        row_size = 3
-        start_of_rows = [0, 3, 6]
-        for i in start_of_rows:
+        for i in self._starts_of_rows:
             self._print_border()
-            self._print_row(cells[i:i + row_size])
+            self._print_row(cells[i:i + self._row_size])
         self._print_border()
 
     def _update_cells(self, player, moves, cells):
