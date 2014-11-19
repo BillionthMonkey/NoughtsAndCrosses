f9e635eedcc138997f5eb9b8fa5d3455c7af0327
Matthew Tylee Atkinson <matatk@gmail.com>
2014-11-19 15:39:54 +0000

    REFACTOR extract number of players field.

--- nac/view.py
+++ nac/view.py
@@ -21,20 +21,22 @@ class NoughtsAndCrossesView():
 
 
 class BoardRenderer():
+    def __init__(self):
+        self._number_of_players = 2
+
     def render(self, moves):
         cells = [' '] * 9
-        number_of_players = 2
         player_one = 0
         player_two = 1
-        self._update_cells(player_one, moves, cells, number_of_players)
-        self._update_cells(player_two, moves, cells, number_of_players)
+        self._update_cells(player_one, moves, cells)
+        self._update_cells(player_two, moves, cells)
         for i in [0, 3, 6]:
             self._print_border()
             self._print_row(cells[i:i + 3])
         self._print_border()
 
-    def _update_cells(self, player, moves, cells, number_of_players):
-        player_moves = moves[player::number_of_players]
+    def _update_cells(self, player, moves, cells):
+        player_moves = moves[player::self._number_of_players]
         symbol = 'O' if player is 0 else 'X'
         for player_move in player_moves:
             cells[player_move] = symbol
