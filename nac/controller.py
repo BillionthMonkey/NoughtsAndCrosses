a027c7237c984aa1633a0b0f3fa11825a6cf1efd
Matthew Tylee Atkinson <matatk@gmail.com>
2014-11-21 13:38:13 +0000

    GREEN

--- nac/controller.py
+++ nac/controller.py
@@ -23,6 +23,6 @@ class NoughtsAndCrossesController:
         self.view.add_move(move)
         if move_result == MoveResult.win:
             self.view.win()
-            self.model.reset()
+            self.reset()
         if move_result == MoveResult.draw:
             self.view.draw()
