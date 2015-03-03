ac29b93507fe4dd1367043ffe0d7214b00be3f41
Matthew Tylee Atkinson <matatk@gmail.com>
2015-03-03 12:50:07 +0000

    REFACTOR Improve design by moving code
    The Rules allow us to move code to improve its structure.  This is not
    really refactoring in the strictest sense, as it changes the behaviour
    of the function (whereas refactoring should preserve behaviour).
    However, the tests all still pass and this has given us a way to improve
    the design.

--- nac.py
+++ nac.py
@@ -10,8 +10,8 @@ def test_illegal_move_is_not_legal():
 
 def test_different_legal_move_is_legal():
     move = 1
-    assert is_legal(move) or move > 0
+    assert is_legal(move)
 
 
 def is_legal(move):
-    return move == 0
+    return move == 0 or move > 0
