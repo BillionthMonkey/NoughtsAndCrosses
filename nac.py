91883826bde501dca30d0b90d3f29bc3a6aeb3d2
Matthew Tylee Atkinson <matatk@gmail.com>
2015-03-03 12:55:14 +0000

    RED Test the first illegal move on upper boundary
    Note: we don't need to test the move '8' as the test would already pass.

--- nac.py
+++ nac.py
@@ -13,5 +13,10 @@ def test_different_legal_move_is_legal():
     assert is_legal(move) is True
 
 
+def test_high_illegal_move_is_not_legal():
+    move = 9
+    assert is_legal(move) is False
+
+
 def is_legal(move):
     return move >= 0
