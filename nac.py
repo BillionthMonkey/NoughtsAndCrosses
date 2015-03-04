b4cd8ff042e41d96417db6e4bac3ed896176e521
Matthew Tylee Atkinson <matatk@gmail.com>
2015-03-04 13:13:29 +0000

    RED Getting moves for player one
    We started thinking that testing for a win (and later other game states)
    was a suitable next step.  However, in order to do that, we realised
    that it would be necessary to know the moves for each player.

--- nac.py
+++ nac.py
@@ -24,5 +24,11 @@ def test_move_is_illegal_if_already_played():
     assert is_legal(move, moves_played) is False
 
 
+def test_moves_for_player_one_are_at_even_indices():
+    moves_played = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    player_one_moves = []
+    assert player_one_moves == [0, 2, 4, 6, 8]
+
+
 def is_legal(move, moves_played):
     return move >= 0 and move < 9 and move not in moves_played
