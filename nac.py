4858ff0ca5a86b6b5b0aafa4627ed8006fe503c1
Matthew Tylee Atkinson <matatk@gmail.com>
2015-03-04 12:56:35 +0000

    REFACTOR Introduce Parameter
    This is an interim step (and also a 'genuine' refactoring) to
    introducing the code from the latest test into the is_legal() function.

--- nac.py
+++ nac.py
@@ -1,28 +1,28 @@
 def test_legal_move_is_legal():
     move = 0
-    assert is_legal(move) is True
+    assert is_legal(move, []) is True
 
 
 def test_illegal_move_is_not_legal():
     move = -1
-    assert is_legal(move) is False
+    assert is_legal(move, []) is False
 
 
 def test_different_legal_move_is_legal():
     move = 1
-    assert is_legal(move) is True
+    assert is_legal(move, []) is True
 
 
 def test_high_illegal_move_is_not_legal():
     move = 9
-    assert is_legal(move) is False
+    assert is_legal(move, []) is False
 
 
 def test_move_is_illegal_if_already_played():
     moves_played = [0]
     move = 0
-    assert (is_legal(move) and move not in moves_played) is False
+    assert (is_legal(move, moves_played) and move not in moves_played) is False
 
 
-def is_legal(move):
+def is_legal(move, moves_played):
     return move >= 0 and move < 9
