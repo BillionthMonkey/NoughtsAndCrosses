568ec426db8cf746ab687216cec3760b26976995
Matthew Tylee Atkinson <matatk@gmail.com>
2015-03-23 13:26:27 +0000

    GREEN

--- nac.py
+++ nac.py
@@ -75,7 +75,7 @@ def test_second_player_wins():
     game_moves = [4, 0, 3, 1, 8, 2]
     player = last_player(game_moves)
     player_moves = moves_for_player(player, game_moves)
-    assert is_win(player_moves) is False
+    assert is_win(player_moves) is True
 
 
 def is_legal(move, moves_played):
